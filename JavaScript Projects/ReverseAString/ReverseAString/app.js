2b9880d5d73573501b2448448ef7521cfc3eb997
IcyCaster <chester.booker.nz@gmail.com>
2017-05-17 15:59:12 +1200

    Added a comment

--- JavaScript Projects/ReverseAString/ReverseAString/app.js
+++ JavaScript Projects/ReverseAString/ReverseAString/app.js
@@ -1,3 +1,6 @@
 'use strict';
 
-console.log('Hello world');
+// Project One
+console.log('Project "Reverse A String"');
+console.log('Enter a string and the program will reverse it and print it out.\n');
+
